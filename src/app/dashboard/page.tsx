bc7e3cc519ee6ef3c8410df515f5aa92de34a61b
alisonBRs <97985844+alisonBRs@users.noreply.github.com>
2023-08-31 16:45:54 -0300

    feat: redirect working!

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,17 +1,42 @@
 "use client";
 
+import { http } from "@/http/axios-response";
+import { log } from "console";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import secureLocalStorage from "react-secure-storage";
 
 export default function Dashboard() {
-  const isValid = true;
-  const router = useRouter();
+  const [user, setUser] = useState("");
 
-  if (!isValid) {
-    return router.replace("/");
+  const { push } = useRouter();
+
+  async function verifyLogin() {
+    try {
+      let token = secureLocalStorage.getItem("item");
+
+      const login = await http.get("/profile", {
+        headers: { authorization: `Bearer ${token}` },
+      });
+
+      setUser(login.data.loggedUser.name);
+
+      if (!login.data.loggedUser.id) {
+        return;
+      }
+    } catch (err: any) {
+      console.log(err);
+      return push("/");
+    }
   }
+
+  useEffect(() => {
+    verifyLogin();
+  }, []);
+
   return (
-    <div>
-      <p>Hello world</p>
+    <div className="w-full h-screen flex justify-center items-center border-solid border-2 border-red-700">
+      <p>Wellcome {user}!</p>
     </div>
   );
 }
